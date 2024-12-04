25be3057661533c0162f60d9fd02c24b1c2cf8c9
Ruochen Miao <74287387+Ruochen1105@users.noreply.github.com>
2024-12-04 09:00:29 -0500

    modify tqdm

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def train_model(model, train_loader, optimizer, scheduler, epochs, device, save_
 
     model.train()
 
-    for epoch in tqdm(range(epochs), desc=f"training", total=epochs):
+    for epoch in range(epochs):
         epoch_loss = 0.0
 
         for batch in tqdm(train_loader, total=len(train_loader)):
