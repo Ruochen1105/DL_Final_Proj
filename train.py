3e49e9e7c752c2352131832c586997e09a25e6b3
gjxue <garygjxue@gmail.com>
2024-12-12 21:59:28 -0500

    Add gradient clipping feature

--- train.py
+++ train.py
@@ -54,6 +54,7 @@ def train_model(model, train_loader, optimizer, scheduler, epochs, device, save_
 
             optimizer.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             epoch_loss += loss.item()
